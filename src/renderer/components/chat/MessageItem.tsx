35d7a5a06c88d8089916b90cb45183134ac6b795
shuyossy <shuyossy220@gmail.com>
2025-04-21 22:49:53 +0900

    chore: display MD

--- src/renderer/components/chat/MessageItem.tsx
+++ src/renderer/components/chat/MessageItem.tsx
@@ -1,10 +1,43 @@
 import React, { forwardRef, memo } from 'react';
 import { Box, Typography, Paper, Avatar, Tooltip } from '@mui/material';
 import { Person as PersonIcon, SmartToy as BotIcon } from '@mui/icons-material';
+// @ts-ignore
+import Markdown from 'react-markdown';
 import { ChatMessage } from '../../types';
 
 interface MessageProps {
   message: ChatMessage;
+}
+
+interface MarkdownComponentProps {
+  children: React.ReactNode;
+}
+
+// カスタムコンポーネントを外で定義
+function CodeBlock({ children }: MarkdownComponentProps) {
+  return (
+    <Box
+      component="code"
+      sx={{
+        whiteSpace: 'pre-wrap',
+        display: 'inline-block',
+        background: 'rgba(0, 0, 0, 0.04)',
+        padding: '2px 4px',
+        borderRadius: 1,
+        fontSize: '0.875em',
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+function MarkdownParagraph({ children }: MarkdownComponentProps) {
+  return (
+    <Typography variant="body1" component="p" sx={{ margin: 0 }}>
+      {children}
+    </Typography>
+  );
 }
 
 const MessageItem = forwardRef<HTMLDivElement, MessageProps>(
@@ -51,14 +84,70 @@ const MessageItem = forwardRef<HTMLDivElement, MessageProps>(
               borderRadius: 2,
             }}
           >
-            <Typography
+            <Box
               sx={{
-                whiteSpace: 'pre-wrap',
-                wordBreak: 'break-word',
+                // マークダウン要素のスタイル調整
+                '& h1, & h2, & h3, & h4, & h5, & h6': {
+                  margin: '0.15rem',
+                  fontWeight: 'bold',
+                  lineHeight: 1.2,
+                },
+                '& ul, & ol': {
+                  margin: 0,
+                  padding: 0,
+                  paddingLeft: '1rem',
+                  '& ul, & ol': {
+                    marginLeft: '0.5rem',
+                  },
+                },
+                '& li': {
+                  margin: 0,
+                  padding: 0,
+                },
+                '& p': {
+                  margin: 0,
+                  padding: 0,
+                },
+                '& pre': {
+                  background: 'rgba(0, 0, 0, 0.04)',
+                  padding: '0.25rem 0.5rem',
+                  borderRadius: 1,
+                  overflowX: 'auto',
+                  margin: 0,
+                  '& code': {
+                    whiteSpace: 'pre',
+                  },
+                },
+                '& :not(pre) > code': {
+                  whiteSpace: 'pre-wrap',
+                  background: 'rgba(0, 0, 0, 0.04)',
+                  padding: '2px 4px',
+                  borderRadius: 1,
+                  fontSize: '0.875em',
+                },
+                '& blockquote': {
+                  margin: 0,
+                  padding: '0.25rem 0.5rem',
+                  borderLeft: '3px solid rgba(0, 0, 0, 0.1)',
+                },
+                // 見出しのサイズ調整
+                '& h1': { fontSize: '1.2em' },
+                '& h2': { fontSize: '1.1em' },
+                '& h3, & h4, & h5, & h6': { fontSize: '1em' },
+                // 段落間の余白調整
+                '& p + p': { marginTop: '0.25rem' },
+                '& pre + p, & p + pre': { marginTop: '0.25rem' },
               }}
             >
-              {message.content}
-            </Typography>
+              <Markdown
+                components={{
+                  p: MarkdownParagraph as any,
+                  code: CodeBlock as any,
+                }}
+              >
+                {message.content}
+              </Markdown>
+            </Box>
           </Paper>
           <Typography
             variant="caption"
